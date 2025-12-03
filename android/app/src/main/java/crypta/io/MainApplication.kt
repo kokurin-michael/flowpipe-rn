0ff03024d578ed183b475c334b688d2081492b71
Mikhail <pierrworking@gmail.com>
2025-12-03 02:25:21 -0800

    feat: переименовал проект; добавил конфигураций в package.json

--- android/app/src/main/java/crypta/io/MainApplication.kt
+++ android/app/src/main/java/crypta/io/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.templateproject
+package crypta.io
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/Crypta/AppDelegate.swift
+++ ios/Crypta/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "TemplateProject",
+      withModuleName: "Crypta",
       in: window,
       launchOptions: launchOptions
     )
